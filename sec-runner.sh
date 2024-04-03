05a8bb82588a223e2c81d3fed8d6a0144129cbde
Jose <jgh9094@gmail.com>
2024-04-03 11:57:13 -0700

    runs for second scorer objective

---
+++ sec-runner.sh
@@ -0,0 +1,68 @@
+#!/bin/bash -l
+
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-120%120
+#SBATCH -t 2:00:00
+#SBATCH --mem=1GB
+#SBATCH --job-name=aqtl
+#SBATCH -p defq,moore
+#SBATCH --exclude=esplhpc-cp040
+
+##################################
+# Setup environment
+##################################
+
+source /home/hernandezj45/anaconda3/etc/profile.d/conda.sh
+conda activate autoqtl-env
+
+##################################
+# Setup random seed info
+##################################
+EXPERIMENT_OFFSET=80
+SEED=$((SLURM_ARRAY_TASK_ID + EXPERIMENT_OFFSET))
+
+##################################
+# Treatments
+##################################
+
+MAX_ERROR__MIN=1
+MAX_ERROR__MAX=40
+
+MEANAE__MIN=41
+MEANAE__MAX=80
+
+MEDIANAE__MIN=81
+MEDIANAE__MAX=120
+
+
+##################################
+# Conditions
+##################################
+
+if [ ${SLURM_ARRAY_TASK_ID} -ge ${MAX_ERROR__MIN} ] && [ ${SLURM_ARRAY_TASK_ID} -le ${MAX_ERROR__MAX} ] ; then
+  SCORER=neg_mean_absolute_error
+  DIR=MeanAE
+
+elif [ ${SLURM_ARRAY_TASK_ID} -ge ${MEANAE__MIN} ] && [ ${SLURM_ARRAY_TASK_ID} -le ${MEANAE__MAX} ] ; then
+  SCORER=neg_median_absolute_error
+  DIR=MedianAE
+
+elif [ ${SLURM_ARRAY_TASK_ID} -ge ${MEDIANAE__MIN} ] && [ ${SLURM_ARRAY_TASK_ID} -le ${MEDIANAE__MAX} ] ; then
+  SCORER=max_error
+  DIR=MaxError
+
+else
+  echo "${SEED} from ${PROBLEM} failed to launch" >> /home/hernandezj45/Repos/GPTP-2O24-FINAL/GPTP-2024-Lexicase-Analysis/Results/failtolaunch.txt
+fi
+
+##################################
+# Data dump directory
+##################################
+
+DATA_DIR=/home/hernandezj45/Repos/AutoQTL-Adventures/Results/${DIR}/${SEED}
+
+python /home/hernandezj45/Repos/AutoQTL-Adventures/selection-diff.py \
+--selection ${SCORER} \
+--seed ${SEED} \
+--savepath ${DATA_DIR}
---
+++ second-scorer.py
@@ -0,0 +1,100 @@
+import pandas as pd # for data loading and manipulation
+from sklearn.model_selection import train_test_split # for splitting the datasets as required
+import matplotlib.pyplot as plt # for visualization
+
+from autoqtl import AUTOQTLRegressor # for running autoqtl
+import numpy as np
+import pandas as pd
+import argparse
+import os
+
+# to run locally and with updated changes: pip install -e ./lexicase-base; clear; python selection-diff.py --selection Lexicase --seed 1 --savepath ./
+
+scoreres = ['neg_mean_absolute_error','neg_median_absolute_error','max_error']
+
+def main():
+    # read in arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scorer",  default=0,  type=int)
+    parser.add_argument("--seed",       default=1,        type=int)
+    parser.add_argument("--savepath",   default="./",     type=str)
+
+    # Parse all the arguments and print
+    args = parser.parse_args()
+    print('scorer:', scoreres[args.scorer])
+    print('seed:', args.seed)
+    print('save path:', args.savepath)
+
+    # read the sample data
+    data = pd.read_csv("sample_data.csv") # change the data path as required
+
+    # extract the features and target
+    features = data.iloc[:,:-1] # all the columns except the last column
+    target = data.iloc[:,-1] # the last column
+
+    # split the data into 80-20, use 20 as holdout and 80 for running autoqtl
+    features_80, features_20, target_80, target_20 = train_test_split(features, target, test_size=0.2, random_state=42)
+
+    # Initializing the autoqtl object using the AUTOQTLRegressor class
+    autoqtl_object = AUTOQTLRegressor(population_size=100, generations=100, verbosity=2, random_state=int(args.seed),
+                                      selection='Lexicase', sec_scoring=scoreres[args.scorer], ss_weight=1.0)
+
+    # calling the fit function
+    autoqtl_object.fit(features= features_80, target= target_80, random_state= 42, test_size= 0.5)
+
+    # get pareto front
+    front = autoqtl_object.get_pareto_front()
+
+    r2 = []
+
+    for pipeline, pipeline_scores in zip(front.items, reversed(front.keys)):
+        r2.append(pipeline_scores.wvalues[0])
+
+    idx = []
+    model = 0
+
+    for pipeline, pipeline_scores in zip(front.items, reversed(front.keys)):
+        if pipeline_scores.wvalues[0] == np.max(r2):
+            idx.append(pipeline)
+
+    diff = []
+    for pipeline, pipeline_scores in zip(front.items, reversed(front.keys)):
+        if pipeline in idx:
+            diff.append(pipeline_scores.wvalues[1])
+
+    final = []
+    for pipeline, pipeline_scores in zip(front.items, reversed(front.keys)):
+        if (pipeline in idx) and (pipeline_scores.wvalues[1] == np.max(diff)):
+            final.append(pipeline)
+
+    if len(final) > 1:
+        model =  np.choose(final)
+    else:
+        model = final[0]
+
+    print('pipeline:', model)
+    print()
+
+    model_id = 1
+    for p in autoqtl_object.get_pareto_front():
+        if p == model:
+            break
+        model_id += 1
+
+    print('model_id:',model_id)
+    holdout_score = autoqtl_object.score_user_choice(features_20, target_20, model_id)
+    print('holdout score:', holdout_score)
+
+    print('CREATING DIRECTORY')
+    os.makedirs(args.savepath)
+    print("SUCCESSFULLY CREATED:",args.savepath)
+
+    autoqtl_object.plot_final_pareto().savefig(args.savepath + '/front.png')
+
+    # save the scores
+    data = [ [args.selection, holdout_score]]
+    df = pd.DataFrame(data, columns=['Scheme', 'Score'])
+    df.to_csv(args.savepath + '/data.csv', index=False)
+
+if __name__ == '__main__':
+    main()
